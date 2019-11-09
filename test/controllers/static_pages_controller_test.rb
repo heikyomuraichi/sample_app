c5ac2b02bb51694a6e210dfe8305e1541c0f4c6e
kyohei <nacsan654@gmail.com>
2019-11-10 03:06:21 +0900

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,4 +1,3 @@
-
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
@@ -19,5 +18,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
